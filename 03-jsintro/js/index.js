20a6f0bc12527c26c7418b2e75f03f6c35c12864
AaronChiappella <aaronchiappella@gmail.com>
2023-11-19 23:59:06 -0300

    js y css

--- 03-jsintro/js/index.js
+++ 03-jsintro/js/index.js
@@ -29,19 +29,76 @@ else { console.log(`La persona ${nombre} ${apellido} posee un estado ${activo}`)
 console.log(`=================ACUMULADORES Y CONTAADORES =====================`)
 let uno = 1;
 
-uno ++;
+uno++;
 console.log(uno);
 
-uno --;
+uno--;
 console.log(uno);
 
 //ACUMULADOR: INCREMENTA Y ASIGNA
-uno +=500;
+uno += 500;
 console.log(uno);
 
-uno +=500;
+uno += 500;
 console.log(uno);
 
+//var nombre = prompt(`Ingresa tu nombre: `); //PROMPT ES UNA INTRODUCCION EN CONSOLA
+//alert(`Hola ${nombre}!`); //ALERT ES UNA ALERTA DE NAVEGADOR 
 
-var nombre = prompt(`Ingresa tu nombre: `); //PROMPT ES UNA INTRODUCCION EN CONSOLA
-alert(`Hola ${nombre}!`); //ALERT ES UNA ALERTA DE NAVEGADOR 
+
+
+console.log(`=================OPERADORES RELACIONALES =====================`)
+
+//let uno = 1;
+let dos = 2;
+
+resultado = dos == uno;
+console.log(resultado);
+
+resultado = dos > uno;
+console.log(resultado);
+
+resultado = dos < uno;
+console.log(resultado);
+
+
+/*
+LOGICOS
+AND (&&):
+
+Devuelve true si ambos operandos son true.
+Ejemplo: a && b
+OR (||):
+
+Devuelve true si al menos uno de los operandos es true.
+Ejemplo: a || b
+NOT (!):
+
+Devuelve true si el operando es false, y viceversa.
+Ejemplo: !a
+
+RELACIONALES
+Igualdad (== o ===):
+
+Desigualdad (!= o !==):
+
+
+Mayor o igual que (>=):
+*/
+
+console.log(`=================CONDICIONAL =====================`)
+//if () {} else {} -> SINTAXIS
+
+var MiEdad = 15;
+
+if (MiEdad >= 18) {
+    console.log("Cliente mayor de edad!");
+}
+else { console.log(`Cliente menor de edad!`); }
+
+
+
+
+
+
+
